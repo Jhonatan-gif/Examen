3285bfd0f47121da7ae123ac779edd54cd29b365
Jhonatan Tipan <jhonatan.tipan@udla.edu.ec>
2023-11-01 16:08:51 -0500

    Pedir cuantos datos van a ingresar

--- Examen.c
+++ Examen.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+
 int main()
 {
+    int cantidad ;
+    int cont=0 ;
+    
+    printf ("Cuantos numeros desea ingresar: "); 
+    scanf ("%d", &cantidad);
+    
+    
 
     return 0;
 }
