c40976a1a3d6a6044695b280a677f5bc91a6d50d
Jhonatan Tipan <jhonatan.tipan@udla.edu.ec>
2023-11-01 16:21:31 -0500

    Uso del ciclo do-while para controlar las repeticiones

--- Examen.c
+++ Examen.c
@@ -5,9 +5,20 @@ int main()
 {
     int cantidad ;
     int cont=0 ;
+    int cont1=0 ; 
+    int num ;
     
     printf ("Cuantos numeros desea ingresar: "); 
     scanf ("%d", &cantidad);
+
+    do{
+    cont1=cont1+1;
+    cont=cont+1;
+    printf("\n Ingrese el numero %d:", cont1 );
+    scanf ("%d",&num);
+
+
+    }while(cont < cantidad);
     
     
 
